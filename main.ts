fe2789054b558d26f803188fff70e24273bbc786
issue4th <44305925+issue4th@users.noreply.github.com>
2022-03-08 06:28:31 +0000

    Latest changes

--- main.ts
+++ main.ts
@@ -1,17 +1,52 @@
+namespace SpriteKind {
+    export const chest = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
+    mySprite2 = sprites.create(img`
+        f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f 
+        e e e e e e f f f f e e e e e e 
+        e e e e e e f f f f e e e e e e 
+        e e e e e e f f f f e e e e e e 
+        e e e e e e e e e e e e e e e e 
+        e e e e 5 e e e 5 5 e e e e e e 
+        e 5 e e 5 5 5 5 5 5 5 5 e e 5 e 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 f f f f f f 5 5 f f 5 5 5 f f 
+        5 e e e e e e 5 5 e e e 5 e e e 
+        e e e e e e e e 5 e e e 5 e e e 
+        5 e e e e e e e e e e e e e e e 
+        e e e e e e e e e e e e e e e e 
+        e e e e e e e e 5 e e e e e e e 
+        e e e e e e e e e e e e e e e e 
+        `, SpriteKind.chest)
+    tiles.placeOnRandomTile(mySprite2, assets.tile`myTile13`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    tiles.loadMap(tiles.createMap(tilemap`level5`))
+    start_level()
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     tiles.loadMap(tiles.createMap(tilemap`level2`))
     start_level()
 })
 function start_level () {
-    tiles.placeOnRandomTile(mySprite, assets.tile`myTile5`)
+    tiles.placeOnRandomTile(player_1, assets.tile`myTile5`)
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, location) {
-    game.over(false, effects.slash)
+    game.over(false, effects.melt)
 })
-let mySprite: Sprite = null
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
+    tiles.placeOnRandomTile(player_1, assets.tile`myTile12`)
+})
+let mySprite2: Sprite = null
+let player_1: Sprite = null
 scene.setBackgroundColor(11)
 tiles.loadMap(tiles.createMap(tilemap`level1`))
-mySprite = sprites.create(img`
+player_1 = sprites.create(img`
     . . . . . . . . . 
     . . . . . . . . . 
     . . . . . f f f . 
@@ -29,6 +64,6 @@ mySprite = sprites.create(img`
     . . . . f . . . f 
     . . . . f . . . f 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite, 50, 50)
-scene.cameraFollowSprite(mySprite)
-tiles.placeOnRandomTile(mySprite, assets.tile`myTile5`)
+controller.moveSprite(player_1, 50, 50)
+scene.cameraFollowSprite(player_1)
+tiles.placeOnRandomTile(player_1, assets.tile`myTile5`)
